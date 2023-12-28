4bfa6c2915ea544a219f12e5831595ab29575cd7
abhishek pokhriyal <abhishek.pokhriyal8@gmail.com>
2023-12-29 01:30:55 +0530

    creating a image slider component

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log("hi");
+
+const currentSlide=(index)=>{
+  showSlide(index);
+}
+
+const showSlide=(index)=>{
+    let slides=document.getElementsByClassName("image__Container");
+    let dots=document.getElementsByClassName("dot");        
+ 
+    for (let ele of slides){
+         
+    }
+}
