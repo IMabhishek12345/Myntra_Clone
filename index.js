bc4a2803f9a4f62af9472d7e9cb1d66d091237c5
abhishek pokhriyal <abhishek.pokhriyal8@gmail.com>
2024-01-03 01:35:19 +0530

    adding a image slider

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 console.log("hi");
+let slideIndex=0;
 
 const currentSlide=(index)=>{
   showSlide(index);
 }
 
 const showSlide=(index)=>{
+    
     let slides=document.getElementsByClassName("image__Container");
-    let dots=document.getElementsByClassName("dot");        
- 
+    let dot=document.getElementsByClassName("dot");        
+    console.log(dot);
     for (let ele of slides){
-         
+        ele.style.display="none"
     }
-}
+    
+    for (let i=0; i<dot.length ; i++){
+      dot[i].className=dot[i].className.replace("active","");
+    }
+          
+    slides[slideIndex].style.display="block";
+    dot[slideIndex].className+="active";
+
+  }
+  showSlide(slideIndex);
